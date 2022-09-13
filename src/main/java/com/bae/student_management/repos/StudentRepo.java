a4ec98990d13d437377b952c008a177f692e387d
mattadej <mattadeju@gmail.com>
2022-09-13 11:46:50 +0100

    Find people by their first name in the database

---
+++ src/main/java/com/bae/student_management/repos/StudentRepo.java
@@ -0,0 +1,14 @@
+package com.bae.student_management.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.bae.student_management.entities.Student;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StudentRepo extends JpaRepository<Student, Long> {
+
+    // Find by firstName. This will return multiple records.
+    List<Student> findStudentByFirstName(String firstName);
+}
