f26b8f5b82a2840edd00124ee2ed0525572dcb86
mattadej <mattadeju@gmail.com>
2022-09-13 09:24:11 +0100

    Added getters and setters

--- src/main/java/com/bae/student_management/entities/Student.java
+++ src/main/java/com/bae/student_management/entities/Student.java
@@ -59,4 +59,56 @@ public class Student {
         this.email = email;
     }
 
+    //Getters and Setters
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+
 }
