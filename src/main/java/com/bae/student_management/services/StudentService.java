615f4b83fc19b4d7aa64dea09a1db6c14a479e34
mattadej <mattadeju@gmail.com>
2022-09-13 10:01:13 +0100

    Added fucntionality to read by ID

--- src/main/java/com/bae/student_management/services/StudentService.java
+++ src/main/java/com/bae/student_management/services/StudentService.java
@@ -30,6 +30,12 @@ public class StudentService {
         return new ResponseEntity<List<Student>>(this.service.getAll(), HttpStatus.OK);
     }
 
+    @GetMapping("/getById/{id}")
+    // @PathVariable allows us to pass a variable (in this case ID) to the path &
+    // service.readById method
+    public ResponseEntity<Student> getById(@PathVariable long id) {
+        return new ResponseEntity<Student>(this.service.getById(id), HttpStatus.OK);
+    }
 
 
 }
