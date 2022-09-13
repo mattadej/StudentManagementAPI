d0ab78af845bf59b2fa733e6db5363d70ea5c211
mattadej <mattadeju@gmail.com>
2022-09-13 10:44:19 +0100

    Added package import to StudentService and create fucntionlity to StudentController

---
+++ src/main/java/com/bae/student_management/controllers/StudentController.java
@@ -0,0 +1,30 @@
+package com.bae.student_management.controllers;
+
+import com.bae.student_management.entities.Student;
+import com.bae.student_management.services.StudentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+
+    private StudentService service;
+
+    public StudentController(StudentService service) {
+        super();
+        this.service = service;
+    }
+
+    // Post requests (CREATE)
+    @PostMapping("/create")
+    // @RequestBody allows us to pass through an object/data when we make the
+    // request
+    public ResponseEntity<Student> create(@RequestBody Student students){
+        return new ResponseEntity<Student>(this.service.create(students), HttpStatus.CREATED);
+    }
+
+}
